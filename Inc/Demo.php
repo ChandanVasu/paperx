435f1fe8b74496a40db62fa32efc423b272bda92
Chandan Vasu <chandanyadavparas@gmail.com>
2024-03-14 19:31:07 +0530

    ad demo server import

--- Inc/Demo.php
+++ Inc/Demo.php
@@ -1,39 +1,23 @@
 <?php
 
-function ocdi_import_files() {
-    return [
-      [
-        'import_file_name'           => 'Demo Import 1',
-        'categories'                 => [ 'Movie' ],
-        'import_file_url'            => 'https://vasutime.com/mjnnu.xml',
-        'import_widget_file_url'     => 'https://vasutime.com/mjnnu.wie',
-        'import_customizer_file_url' => 'https://vasutime.com/paper.dat',
-        
-        'import_preview_image_url'   => 'https://user-images.githubusercontent.com/106135144/196727097-50c0ae49-b92f-4aa9-bdcb-30d978a44125.png',
-        'preview_url'                => 'https://mjnnu.com/mj/',
-      ],
-      [
-        'import_file_name'           => 'Demo Import 1',
-        'categories'                 => [ 'News' ],
-        'import_file_url'            => 'https://vasutime.com/mjnnu.xml',
-        'import_widget_file_url'     => 'https://vasutime.com/mjnnu.wie',
-        'import_customizer_file_url' => 'https://vasutime.com/paper.dat',
-        
-        'import_preview_image_url'   => 'https://themesvillage.com/wp-content/uploads/2020/11/Screenshot-2020-11-20-100407.png',
-        'preview_url'                => 'https://mjnnu.com/mj/',
-      ],
-      [
-        'import_file_name'           => 'Demo Import 1',
-        'categories'                 => [ 'News' ],
-        'import_file_url'            => 'https://vasutime.com/mjnnu.xml',
-        'import_widget_file_url'     => 'https://vasutime.com/mjnnu.wie',
-        'import_customizer_file_url' => 'https://vasutime.com/paper.dat',
-        
-        'import_preview_image_url'   => 'https://themesvillage.com/wp-content/uploads/2020/11/Screenshot-2020-11-20-100407.png',
-        'preview_url'                => 'https://mjnnu.com/mj/',
-      ],
+function ocdi_import_files_from_json_url() {
+  // URL to JSON file
+  $json_url = 'https://mjnnu.com/demo/data.json';
 
-    ];
+  // Fetch JSON data from URL
+  $json_data = file_get_contents($json_url);
+
+  // Decode JSON data
+  $import_data = json_decode($json_data, true);
+
+  // Check if JSON decoding was successful
+  if ($import_data === null) {
+      // JSON decoding failed, handle error here
+      return [];
   }
-  add_filter( 'ocdi/import_files', 'ocdi_import_files' );
 
+  // Return the imported data
+  return $import_data;
+}
+
+add_filter('ocdi/import_files', 'ocdi_import_files_from_json_url');
